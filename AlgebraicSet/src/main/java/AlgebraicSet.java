b79cf41fec42a87ad7fee7c3ee2ef9613be4a1f4
GinzaJason <33102169+ginzajason@users.noreply.github.com>
2019-11-16 11:51:05 -0500

    Basically done just need to add comments

--- AlgebraicSet/src/main/java/AlgebraicSet.java
+++ AlgebraicSet/src/main/java/AlgebraicSet.java
@@ -1,15 +1,59 @@
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class AlgebraicSet implements Set<Object>{
-	Set<Object> thisSet;
+/*
+ * AlgebraicSet.java -- Can take the intersection, union, and difference of two Sets
+ * Jason Melnik
+ * 11/16/2019
+ */
+public class AlgebraicSet<T> implements Set<T>{
+	Set<T> thisSet;
 	
-	public AlgebraicSet(Set<Object> start) {
+	public AlgebraicSet(Set<T> start) {
 		thisSet = start;
 	}
 	
 	public AlgebraicSet() {
+		thisSet = new HashSet<T>();
+	}
+	
+	public Set<T> intersect(Set<T> inp) {
+		Set<T> intersect = new HashSet<T>();
+		
+		T[] input = (T[]) inp.toArray();
+		
+		for(int x = 0; x < input.length; x++) {
+			if(thisSet.contains(input[x])) {
+				intersect.add(input[x]);
+			}
+		}
+		
+		return intersect;
+	}
+	
+	public Set<T> union(Set<T> inp) {
+		T[] input = (T[]) inp.toArray();
+		Set<T> union = new HashSet<T>(thisSet);
+		
+		for(int x = 0; x < input.length; x++) {
+			union.add(input[x]);
+		}
+		
+		return union;
+	}
+	
+	public Set<T> difference(Set<T> inp) {
+		Set<T> difference = new HashSet<T>();
+		T[] thisArr = (T[]) thisSet.toArray();
+		
+		for(int x = 0; x < thisArr.length; x++) {
+			if(!inp.contains(thisArr[x])) {
+				difference.add(thisArr[x]);
+			}
+		}
+		return difference;
 	}
 	
 	public int size() {
@@ -27,7 +71,7 @@ public class AlgebraicSet implements Set<Object>{
 		return thisSet.contains(o);
 	}
 
-	public Iterator<Object> iterator() {
+	public Iterator<T> iterator() {
 		// TODO Auto-generated method stub
 		return thisSet.iterator();
 	}
@@ -42,7 +86,7 @@ public class AlgebraicSet implements Set<Object>{
 		return thisSet.toArray(a);
 	}
 
-	public boolean add(Object e) {
+	public boolean add(T e) {
 		// TODO Auto-generated method stub
 		return thisSet.add(e);
 	}
@@ -52,28 +96,35 @@ public class AlgebraicSet implements Set<Object>{
 		return thisSet.remove(o);
 	}
 
-	public boolean containsAll(Collection<?> c) {
+	public void clear() {
+		thisSet.clear();
+	}
+
+	public boolean containsAll(Collection c) {
 		// TODO Auto-generated method stub
 		return thisSet.containsAll(c);
 	}
 
-	public boolean addAll(Collection<?> c) {
+	public boolean addAll(Collection c) {
 		// TODO Auto-generated method stub
 		return thisSet.addAll(c);
 	}
 
-	public boolean retainAll(Collection<?> c) {
+	public boolean retainAll(Collection c) {
 		// TODO Auto-generated method stub
 		return thisSet.retainAll(c);
 	}
 
-	public boolean removeAll(Collection<?> c) {
+	public boolean removeAll(Collection c) {
 		// TODO Auto-generated method stub
-		return thisSet.removeAll(c);
+		return thisSet.retainAll(c);
 	}
-
-	public void clear() {
-		thisSet.clear();
+	
+	public String toString() {
+		HashSet temp = (HashSet) thisSet;
+		
+		return temp.toString();
+		
 	}
 	
 }
--- AlgebraicSet/src/test/java/AlgebraicSetTest.java
+++ AlgebraicSet/src/test/java/AlgebraicSetTest.java
@@ -4,28 +4,205 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+/*
+ * AlgebraicSetTest.java -- This tests the intersection, union, and difference of two Sets
+ * Jason Melnik
+ * 11/16/2019
+ */
 class AlgebraicSetTest {
 
-	private Set<Character> mathSet;
-	
+	private AlgebraicSet<Character> mathSet;
+	private Set<Character> testSet;
 	
 	@BeforeEach
 	void initEach() {
-		//mathSet = new AlgebraicSet();
-	}
-	
-	@Test
-	void test() {
-		Set<Character> testSet = new HashSet<Character>();
+		testSet = new HashSet<Character>();
 		testSet.add('A');
 		testSet.add('B');
 		testSet.add('C');
 		testSet.add('D');
 		testSet.add('E');
-		mathSet = testSet;
-		assertEquals(testSet, testSet);
+		
+		mathSet = new AlgebraicSet<Character>();
+		mathSet.add('A');
+		mathSet.add('B');
+		mathSet.add('C');
+		mathSet.add('D');
+		mathSet.add('E');
+	}
+	
+	@Nested
+	class testIntersect{
+		
+		@Test
+		void noChange() {
+			assertEquals(testSet, mathSet.intersect(testSet));
+		}
+		
+		@Test
+		void adding1Letter() {
+			mathSet.add('F');
+			assertEquals(testSet, mathSet.intersect(testSet));
+		}
+		
+		@Test
+		void adding2Letter() {
+			mathSet.add('F');
+			mathSet.add('G');
+			assertEquals(testSet, mathSet.intersect(testSet));
+		}
+		
+		@Test
+		void removing1Letter() {
+			mathSet.remove('E');
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.remove('E');
+			assertEquals(tempTest, mathSet.intersect(testSet));
+		}
+		
+		@Test
+		void removing2Letters() {
+			mathSet.remove('E');
+			mathSet.remove('D');
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.remove('E');
+			tempTest.remove('D');
+			assertEquals(tempTest, mathSet.intersect(testSet));
+		}
+	}
+	
+	@Nested
+	class testUnion{
+		
+		@Test
+		void noChange() {
+			assertEquals(testSet, mathSet.union(testSet));
+		}
+		
+		@Test
+		void adding1Letter() {
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.add('F');
+			mathSet.add('F');
+			assertEquals(tempTest, mathSet.union(testSet));
+		}
+		
+		@Test
+		void adding2Letters() {
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.add('F');
+			tempTest.add('G');
+			mathSet.add('F');
+			mathSet.add('G');
+			assertEquals(tempTest, mathSet.union(testSet));
+		}
+		
+		@Test
+		void removing1Letter() {
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.remove('F');
+			mathSet.remove('F');
+			assertEquals(tempTest, mathSet.union(testSet));
+		}
+		
+		@Test
+		void removing2Letters() {
+			Set<Character> tempTest = new HashSet<Character>(testSet);
+			tempTest.remove('F');
+			tempTest.remove('G');
+			mathSet.remove('F');
+			mathSet.remove('G');
+			assertEquals(tempTest, mathSet.union(testSet));
+		}
+	}
+	
+	@Nested
+	class testDifference{
+		
+		@Test
+		void noChange() {
+			Set<Character> tempTest = new HashSet<Character>();
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void adding1LetterToA() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.add('F');
+			tempTest.add('F');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void adding2LettersToA() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.add('F');
+			mathSet.add('G');
+			tempTest.add('F');
+			tempTest.add('G');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void adding1LetterToB() {
+			Set<Character> tempTest = new HashSet<Character>();
+			testSet.add('F');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void adding2LettersToB() {
+			Set<Character> tempTest = new HashSet<Character>();
+			testSet.add('F');
+			testSet.add('G');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void add1ToARemove1ToB() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.add('F');
+			testSet.remove('E');
+			tempTest.add('E');
+			tempTest.add('F');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void add2ToARemove2ToB() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.add('F');
+			mathSet.add('H');
+			testSet.remove('E');
+			testSet.remove('D');
+			tempTest.add('F');
+			tempTest.add('H');
+			tempTest.add('E');
+			tempTest.add('D');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void add1ToBRemove1ToA() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.remove('E');
+			testSet.add('G');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
+		@Test
+		void add2ToBRemove2ToA() {
+			Set<Character> tempTest = new HashSet<Character>();
+			mathSet.remove('E');
+			mathSet.remove('D');
+			testSet.add('G');
+			testSet.add('I');
+			assertEquals(tempTest, mathSet.difference(testSet));
+		}
+		
 	}
 
 }
