ee5c06aaa84f8bdbb926d96a9f0f3d780e1ca7e2
melni <melni@10.9.106.12>
2019-11-17 01:39:36 -0500

    added a javadoc

---
+++ AlgebraicSet/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"AlgebraicSetMain","l":"AlgebraicSetMain()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"AlgebraicSetMain","l":"main(String[])","url":"main(java.lang.String[])"}]
